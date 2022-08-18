6e8bc3ff32a568841e95f37ff4d7133132fb3b16
Wampirs <79167501+Wampirs@users.noreply.github.com>
2022-08-18 11:34:12 +0300

    RelayCommand, vm change

--- ViewModels/MainWindowVM.cs
+++ ViewModels/MainWindowVM.cs
@@ -1,23 +1,50 @@
-﻿using CCExchange.Models;
+﻿using CCExchange.Commands;
 using CCExchange.Services;
 using CCExchange.ViewModels.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CCExchange.ViewModels
 {
     public class MainWindowVM : ViewModel
     {
-        private readonly IApiService api;
+        private TopCurrencyVM topVm;
+        private DetailInfoVM detailVM;
 
-        public MainWindowVM(IApiService _api)
+        private ViewModel curVm;
+        public ViewModel CurrentVm
         {
-            api = _api;
+            get { return curVm; }
+            private set { Set(ref curVm, value); }
         }
-        public List<Currency> Curs => api.GetCurrenciesAsync().Result;
 
+        public MainWindowVM()
+        {
+            topVm = new TopCurrencyVM();
+            detailVM = new DetailInfoVM();
+            CurrentVm = topVm;
+        }
+
+        #region ShowTopCommand
+        private ICommand showTopCommand;
+        public ICommand ShowTopCommand => showTopCommand ??= new RelayCommand(OnShowTopExecuted, CanShowTopExetute);
+        private void OnShowTopExecuted(object o)
+        {
+            if (CurrentVm == topVm) return;
+            CurrentVm = topVm;
+        }
+        private bool CanShowTopExetute(object o) => CurrentVm != topVm;
+
+        #endregion
+        #region ShowDetailCommand
+        private ICommand showdetailCommand;
+        public ICommand ShowDetailCommand => showdetailCommand ??= new RelayCommand(OnShowDetailExecuted, CanShowDetailExetute);
+        private void OnShowDetailExecuted(object o)
+        {
+            if (CurrentVm == detailVM) return;
+            CurrentVm = detailVM;
+        }
+        private bool CanShowDetailExetute(object o) => CurrentVm != detailVM;
+
+        #endregion
     }
 }
