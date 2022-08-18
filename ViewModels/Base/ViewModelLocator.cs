182e5edaa7e4c6b71447ca89cc20708eeffc343e
Wampirs <79167501+Wampirs@users.noreply.github.com>
2022-08-18 13:47:06 +0300

    Currency model setters and datagrid

--- ViewModels/Base/ViewModelLocator.cs
+++ ViewModels/Base/ViewModelLocator.cs
@@ -4,10 +4,10 @@ namespace CCExchange.ViewModels.Base
 {
     public class ViewModelLocator
     {
-        #region MainWindowVM
         MainWindowVM mainWindowVM;
         public MainWindowVM MainWindowVM => mainWindowVM ??= App.Services.GetRequiredService<MainWindowVM>();
-        #endregion
+
+
 
     }
 }
--- ViewModels/TopCurrencyVM.cs
+++ ViewModels/TopCurrencyVM.cs
@@ -1,6 +1,10 @@
-﻿using CCExchange.ViewModels.Base;
+﻿using CCExchange.Models;
+using CCExchange.Services;
+using CCExchange.ViewModels.Base;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +13,18 @@ namespace CCExchange.ViewModels
 {
     public class TopCurrencyVM : ViewModel
     {
+        private readonly IApiService api;
+        private ObservableCollection<Currency> curs;
+        public ObservableCollection<Currency> Curs 
+        {
+            get { return curs; } 
+            private set { Set(ref curs, value); }
+        }
 
+        public TopCurrencyVM()
+        {
+            api = App.Services.GetRequiredService<IApiService>();
+            Curs = new ObservableCollection<Currency>(api.GetCurrenciesAsync().Result.GetRange(0,10));
+        }
     }
 }
