c46015eb17e64f40a0dd3a9d5390eb707e9f54b5
Wampirs <79167501+Wampirs@users.noreply.github.com>
2022-08-19 21:08:16 +0300

    Added filter

--- ViewModels/DetailInfoVM.cs
+++ ViewModels/DetailInfoVM.cs
@@ -3,20 +3,24 @@ using CCExchange.Services;
 using CCExchange.ViewModels.Base;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CCExchange.ViewModels
 {
     public class DetailInfoVM : ViewModel
     {
         private IApiService api;
-        public CurrencyInfoVM CurrencyInfoVM { get; private set; }
         private List<Currency> currencies;
-        public List<Currency> Currencies
+        private List<Currency> filteredCurrencies;
+        public List<Currency> FilteredCurrencies
         {
-            get => currencies;
-            set => Set(ref currencies, value);
+            get => filteredCurrencies;
+            set
+            {
+                Set(ref filteredCurrencies, value);
+                SelectedCurrency = filteredCurrencies.FirstOrDefault();
+            }
         }
-
         private Currency selectedCurrency;
         public Currency SelectedCurrency
         {
@@ -28,18 +32,46 @@ namespace CCExchange.ViewModels
             }
         }
 
+        private string searchCriteria = string.Empty;
+        public string SearchCriteria
+        {
+            get { return searchCriteria; }
+            set
+            {
+                Set(ref searchCriteria, value);
+                if (FilterChanged != null) FilterChanged();
+            }
+        }
+
+
+
+
 
         public DetailInfoVM()
         {
             api = App.Services.GetRequiredService<IApiService>();
-            CurrencyInfoVM = new CurrencyInfoVM();
-            Currencies = api.GetCurrenciesAsync().Result;
-            SelectedCurrency = Currencies[0];
+            currencies = api.GetCurrenciesAsync().Result;
+            FilterChanged += OnFilterChanged;
+            if (FilterChanged != null) FilterChanged();
         }
 
         private void RefreshSelectedCurrency(Currency curr)
         {
+            if (curr == null) return;
             Set(ref curr, api.GetCurrencyAsync(curr.Id).Result);
         }
+
+        private void OnFilterChanged()
+        {
+            if (SearchCriteria == string.Empty)
+            {
+                FilteredCurrencies = currencies;
+                return;
+            }
+            FilteredCurrencies = currencies.Where(x => x.Name.ToLower().Contains(SearchCriteria.ToLower()) || x.Symbol.ToLower().Contains(SearchCriteria.ToLower())).ToList();
+        }
+
+        private delegate void FilterChangedHandler();
+        private FilterChangedHandler FilterChanged;
     }
 }
